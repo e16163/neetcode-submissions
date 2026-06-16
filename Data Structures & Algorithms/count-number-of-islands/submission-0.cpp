a6e238f9ffe2f15e557f31406a41616cd86ff787
atintw <117666476+e16163@users.noreply.github.com>
2026-06-16 12:54:21 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int num = 0;
+        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
+        vector<vector<vector<char>>> adjacent(grid.size(), vector<vector<char>>(grid[0].size(), vector<char>(4, '0')));
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (i - 1 >= 0)
+                {
+                    adjacent[i][j][0] = grid[i - 1][j];
+                }
+                if (j + 1 < grid[0].size())
+                {
+                    adjacent[i][j][1] = grid[i][j+1];
+                }
+                if (i + 1 < grid.size())
+                {
+                    adjacent[i][j][2] = grid[i+1][j];
+                }
+                if (j - 1 >= 0)
+                {
+                    adjacent[i][j][3] = grid[i][j-1];
+                }
+                
+            }
+        }
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j]=='1' && visited[i][j]==0)
+                {
+                    
+                    dfs(grid, visited, adjacent, num, i, j);
+                    num++;
+                }
+            }
+        }
+        return num;
+        
+
+    }
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& visited, vector<vector<vector<char>>> adjacent, int& num, int i, int j)
+    {
+        
+        visited[i][j] = 1;
+        if (adjacent[i][j][0]=='1' && visited[i-1][j]==0)
+        {
+            
+            dfs(grid, visited, adjacent, num, i - 1, j);
+        }
+        if (adjacent[i][j][1]=='1' && visited[i][j+1]==0)
+        {
+           
+            dfs(grid, visited, adjacent, num, i, j + 1);
+        }
+        if (adjacent[i][j][2]=='1' && visited[i+1][j]==0)
+        {
+           
+            dfs(grid, visited, adjacent, num, i + 1, j);
+        }
+        if (adjacent[i][j][3]=='1' && visited[i][j - 1]==0)
+        {
+            
+            dfs(grid, visited, adjacent, num, i, j - 1);
+        }
+        return;
+    }
+};
